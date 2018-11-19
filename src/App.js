b5a7940cf8408165118eed34ccaa7ba41f1ab72c
sarahcheatham <sarah.cheatham14@gmail.com>
2018-11-19 15:15:53 -0600

    username and password inputs created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header.js';
+import LoginForm from './components/login-form.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>arraynge</header>
+        <Header/>
+        <LoginForm/>
       </div>
     );
   }
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Header extends React.Component{
+   
+    render(){
+        
+        return(
+            <h1 className="headerHome">
+                [arraynge]
+            </h1>
+        ) 
+    }
+}

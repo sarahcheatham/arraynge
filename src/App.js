58c8de54c8cc1adcdcc78d8e23fbc5707e805630
sarahcheatham <sarah.cheatham14@gmail.com>
2018-11-25 16:43:53 -0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Login from './components/login';
+import Navbar from './components/navbar';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <Header/>
+        <Navbar/>
         <Login/>
       </div>
     );
